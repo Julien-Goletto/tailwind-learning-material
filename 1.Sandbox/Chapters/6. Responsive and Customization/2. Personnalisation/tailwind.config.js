e73e38e0a89a76efcc222258a65374875650d47e
Julien GOLETTO <julien.goletto@gmail.com>
2022-11-02 17:55:33 +0100

    :sparkles: more personalization & adding plugins

--- 1.Sandbox/Chapters/6. Responsive and Customization/2. Personnalisation/tailwind.config.js
+++ 1.Sandbox/Chapters/6. Responsive and Customization/2. Personnalisation/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    // colors: {
+    //   'orange': '#ffa500',
+    // },
+    extend: {
+      colors: {
+        'orange': '#ffa500',
+        'bleu_nuit':{
+          700: '#0077b6',
+          800: '#023e8a',
+          900: '#03045e',
+        }
+      },
+      fontSize: {
+        'custom': '70px',
+        'clamp-sm': 'clamp(20px, 3vw, 50px)',
+      },
+      fontFamily: {
+        'open-sans': 'open sans',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")
+  ],
 }
