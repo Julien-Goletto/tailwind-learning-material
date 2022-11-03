3486717fae6e680ab1f0d92d742564d2f590e381
Julien GOLETTO <julien.goletto@gmail.com>
2022-11-03 11:15:12 +0100

    :sparkles: Animations & transitions
    + custom animation handling

--- 1.Sandbox/Chapters/7. Core Concepts TailwindCSS/tailwind.config.js
+++ 1.Sandbox/Chapters/7. Core Concepts TailwindCSS/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./pages/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade": "fade 3s ease-out"
+      },
+      keyframes: {
+        fade: {
+          "0%, 100%": {opacity: 1},
+          "50%": {opacity: 0}
+        }
+      }
+    },
   },
   plugins: [],
 }
